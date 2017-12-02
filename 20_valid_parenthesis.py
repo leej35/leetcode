808c573fc5105062d246beffa6f6c651f5232066
Jeongmin Lee <thelab@naver.com>
2017-12-02 15:02:08 -0500

    Create 20_valid_parenthesis.py

---
+++ 20_valid_parenthesis.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) % 2 != 0: return False
+        dict = {"]":"[", ")":"(", "}":"{"}
+        stack = []
+        
+        for char in list(s):
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
