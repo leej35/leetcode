f7be9e4ef53c34bbc0011c5ff1a5df16a3721aee
Jeongmin Lee <jlee@cs.pitt.edu>
2020-05-21 21:04:23 -0400

    Update easy-array-best-time-to-buy-and-sell.py

--- easy-array-best-time-to-buy-and-sell.py
+++ easy-array-best-time-to-buy-and-sell.py
@@ -1,27 +1,9 @@
 # https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
 
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        state = False  # False: not buy yet, True: already purchased
-        value = 0  # value of current stock
-        ptr = 1
-        profit = 0
-        while(ptr < len(prices)):
-            
-            if not state and prices[ptr-1] < prices[ptr]:
-                # not purchased yet & yesterday was cheaper => buy!
-                value = prices[ptr-1]
-                state = True
-            
-            if state and value < prices[ptr]:
-                # already purchased & today is expensive => sell!
-                profit += prices[ptr] - value
-                state = False
-            
-            ptr += 1
-            
-        return profit
+def maxProfit(prices):
+    max_profit, min_price = 0, float('inf')
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
